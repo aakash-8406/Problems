483a021def28411398da2468f8494d7caa722683
aakash-8406 <aakashmahendran2006@gmail.com>
2026-06-06 18:17:24 +0530

    updated

--- run_length_decoding.java
+++ run_length_decoding.java
@@ -3,15 +3,16 @@ public class run_length_decoding {
         String s="aaabbccdfaa";
         int n=s.length();
         String res="";
+        int count=1;
         for(int i=0;i<n;i++){
-
-            int count=1;
-            while(i < n-1 && s.charAt(i)==s.charAt(i+1)){
+            if(i<n-1 && s.charAt(i)==s.charAt(i+1)) {
                 count++;
-                i++;
+            }else{
+                res+=count+""+s.charAt(i-1);
+                count=1;
             }
-            res+=count+""+s.charAt(i);
         }
         System.out.println(res);
     }
 }
+ 
